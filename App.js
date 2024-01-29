6394f59c90eeebdfd75563c0a88405a48b47d1be
SubhajitD <subhajitdebnath.999@gmail.com>
2024-01-29 17:17:33 +0530

    update react dom

--- App.js
+++ App.js
@@ -1,5 +1,16 @@
-const heading = React.createElement("h1", {
-    id: "heading", abc: "zyx"
-}, "Hello World");
+// const heading = React.createElement("h1", {
+//     id: "heading", abc: "zyx"
+// }, "Hello World");
+
+const parent = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child1"
+    }, 'Child 1'),
+    React.createElement("div", {
+        id: "child2"
+    }, 'Child 2')
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
