0b2e3cb6083caa53e69a54d4f0715ec6fb060851
SubhajitD <subhajitdebnath.999@gmail.com>
2024-01-29 16:04:33 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {
+    id: "heading", abc: "zyx"
+}, "Hello World");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
