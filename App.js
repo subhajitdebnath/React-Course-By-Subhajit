82ddebe7acac68499187aea72090554c81127e5c
SubhajitD <subhajitdebnath.999@gmail.com>
2024-01-29 20:43:43 +0530

    igniting our app

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
-// const heading = React.createElement("h1", {
-//     id: "heading", abc: "zyx"
-// }, "Hello World");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement("div", {
-    id: "parent"
-}, [
-    React.createElement("div", {
-        id: "child1"
-    }, 'Child 1'),
-    React.createElement("div", {
-        id: "child2"
-    }, 'Child 2')
-]);
+// const parent = React.createElement("div", { id: "parent"}, [
+//     React.createElement("div", {
+//         id: "child1"
+//     }, 'Child 1'),
+//     React.createElement("div", {
+//         id: "child2"
+//     }, 'Child 2')
+// ]);
+
+const parent = <h1>hi</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
