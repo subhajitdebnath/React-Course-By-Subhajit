72a6f5fb34113b78109b18201b39e5729588a814
SubhajitD <subhajitdebnath.999@gmail.com>
2024-02-03 13:06:43 +0530

    ep 3: foundation

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// const parent = React.createElement("div", { id: "parent"}, [
-//     React.createElement("div", {
-//         id: "child1"
-//     }, 'Child 1'),
-//     React.createElement("div", {
-//         id: "child2"
-//     }, 'Child 2')
-// ]);
+const parent = React.createElement('h1', {}, 'test');
+console.log(parent);
 
-const parent = <h1>hi</h1>;
+const span = '200';
+const Title = <h1>{span}Hello title</h1>;
+const Child = () => <h1>Hello comp</h1>;
+
+const Child1 = () => (
+    <div>
+        {Child()}
+        <Child />
+        <Child></Child>
+        <h1>Hello comp 1</h1>
+    </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<Child1 />);
